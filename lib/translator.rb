ec31c31b8ffc33e489f740aeb87ec5190dc608ba
david <dkim187@binghamton.edu>
2020-06-25 20:56:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,33 @@
-# require modules here
+require "pry"
 
-def load_library
-  # code goes here
+require "yaml"
+
+
+
+def load_library(file_path)
+  emotocons = YAML.load_file(file_path)
+  new_emotocons = {}
+  emotocons.each do |key, value|
+    new_emotocons[key] = {}
+    new_emotocons[key][:english] = value[0]
+    new_emotocons[key][:japanese] = value[1]
+  end
+  new_emotocons
 end
 
-def get_japanese_emoticon
-  # code goes here
+
+
+
+def get_japanese_emoticon(file_path, emoticon)
+  emoticons = load_library(file_path)
+  emoticons.each do
+    if emotocons[0] == emoticon
+      emotocons[1]
+    end
+  end
 end
+
+
 
 def get_english_meaning
-  # code goes here
 end
